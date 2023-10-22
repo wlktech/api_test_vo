6880f792609a808b8ea6c9c71b76adc021816287
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-10-22 20:09:34 +0630

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,67 +56,23 @@ $('.responsive1').slick({
         {
         breakpoint: 1024,
         settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+            },
+        {
+        breakpoint: 600,
+        settings: {
             slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-        }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-            },
-        {
-        breakpoint: 600,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-        },
-        {
-        breakpoint: 480,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-        }
-    ]
-});
-
-$('.responsive2').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    prevArrow: '<i class="fas fa-angle-left left_arrow2">',
-    nextArrow: '<i class="fas fa-angle-right right_arrow2">',
-    // rtl: true,
-    responsive: [
-        {
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-        }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-            },
-        {
-        breakpoint: 600,
-        settings: {
-            slidesToShow: 1,
             slidesToScroll: 1
         }
         },
